1839cf039f4adb792e00d38a0f4a4155f7ca08ee
cjmpena <98791216+cjmpena@users.noreply.github.com>
2023-04-15 00:47:53 -0500

    Update search.php
    This is a new file called search.php it is a separate page after a user searches for a shoe, it will direct them to this PHP page displaying the main header of the website and the footer to access the other pages. Mainly this shows the actual results of the search made by the user to indicate which keywords matched that of the characters the user typed into the search bar. If results match any characters the links to the shoes will be displayed and the user can access these shoes one by one due to the linked fullpost.php file attached to each true result.

--- search.php
+++ search.php
@@ -1,3 +1,102 @@
 <?php
+/*******w******** 
+    
+    Name: Carla Manansala
+    Date: April 14 2023
+    Description: The search file for my final Project.
+****************/
+
+require('connect.php');
+
+$query = "SELECT * FROM shoecategory";
+$statement = $db->prepare($query);
+$statement->execute();
+
+$keyword  = filter_input(INPUT_POST, 'keyword', FILTER_SANITIZE_STRING);
+$shoecategory = filter_input(INPUT_POST, 'shoecategory', FILTER_VALIDATE_INT);
+
+if ($_POST['shoecategory'] !== '') {
+    $query     = "SELECT * FROM shoes WHERE category_id = '$shoecategory' AND headline LIKE '%{$keyword}%'";
+    $statement = $db->prepare($query);
+} else {
+    $query     = "SELECT * FROM shoes WHERE headline LIKE '%{$keyword}%'";
+    $statement = $db->prepare($query);
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="css/stylesheet/style.css" rel="stylesheet">
+  <title>Shoe Search Results</title>
+</head>
+<body>
+<body>
+    <header id="header" class="fixed-top header-inner-pages">
+        <div class="container d-flex align-items-center justify-content-between">
+        <h1 class="logo"><a href="index.php">John's ShoeBuzz Shop</a></h1>
+        <nav id="navbar" class="navbar">
+            <ul class="nav-menu">
+            <li><a href="index.php">Main BuzzPage</a></li>
+            <li class="dropdown"><a href="#"><span>ShoeShop</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                <li><a href="shoeshop.php">ShoeShop Page</a></li>
+                <li><a href="shoecategories.php">Categories</a></li>
+                <li><a href="sizing.php">Sizing Comparison</a></li>
+                </ul>
+            </li>
+            <li class="dropdown"><a href="#"><span>BuzzFeedback</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                <li><a href="index.php#reviews">Reviews and Comments</a></li>
+                <li><a href="review.php">Leave a Review or Comment!</a></li>
+                </ul>
+            </li>
+            <li><a href="index.php#contact">Questions? Send us A Message!</a></li>
+            <li><a href="#footer">BuzzStaff Contact Information</a></li>
+            <li class="dropdown"><a href="#"><span>Admin</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                <li><a href="post.php">Post For ShoeShop</a></li>
+                <li><a href="categories.php">Add ShoeCategories</a></li>
+                </ul>
+            </li>
+            </ul>
+        </nav>
+        </div>
+    </header>
+    <div class="section-title">
+        <?php if ($statement->execute()): ?>
+            <h1>'<?= $keyword ?>' has <?= $statement->rowCount() ?> result(s).</h1>
+            <?php while($row = $statement->fetch()): ?>
+                <br><h2><a href="fullpost.php?id=<?= $row['id'] ?>"><?= $row['headline'] ?></a></h2><br>
+            <?php endwhile ?>
+        <?php endif ?>
+    </div>
+    <footer id="footer">
+    <div class="footer-top">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-xl-10">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 footer-contact">
+                            <h4>Contact BuzzStaff</h4>
+                            <p>
+                            123 Exchange Street <br>
+                            Winnipeg, M.B <br>
+                            Canada <br><br>
+                            <strong>Phone:</strong> +123 456 7891<br>
+                            <strong>Email:</strong> info@john.com<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </footer>
+</body>
+</html>
