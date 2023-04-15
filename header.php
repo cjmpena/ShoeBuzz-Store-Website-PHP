aaea2686a01cd230cf208a1a68ee213adb87bceb
cjmpena <98791216+cjmpena@users.noreply.github.com>
2023-04-15 00:44:04 -0500

    Create header.php
    This is a new PHP file that displays and functions as the search bar and button for users to type in and search for shoes.

---
+++ header.php
@@ -0,0 +1,34 @@
+<?php
+/*******w******** 
+    
+    Name: Carla Manansala
+    Date: April 14 2023
+    Description: The shoeshop file for my final Project.
+****************/
+$query = "SELECT * FROM shoecategory";
+$statement = $db->prepare($query);
+$statement->execute();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="css/stylesheet/style.css" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <div class="header-nav">
+        <form action="search.php" method="post" class="search-container">
+            <input type="text" name="keyword" placeholder="Search for shoes...">
+            <select name="shoecategory">
+                <option value="">All categories</option>
+                <?php while($category = $statement->fetch()): ?>
+                <option value="<?= $category['id'] ?>"><?= $category['name'] ?></option>
+                <?php endwhile ?>
+            </select>
+            <button type="submit">Search</button>
+        </form>
+        </div>
+    </nav>
+</body>
+</html>
